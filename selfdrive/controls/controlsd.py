7615f1fa80361a72ac0d1c3361087c046b60ae32
Viktor C <vc@abledistributors.com>
2024-07-10 15:39:46 -0500

    DM/Logging OFF

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -100,6 +100,9 @@ class Controls:
       ignore += ['roadCameraState', 'wideRoadCameraState']
     if FrogPilotVariables.toggles.radarless_model:
       ignore += ['radarState']
+
+    ignore += ['driverMonitoringState','driverCameraState']
+
     self.sm = messaging.SubMaster(['deviceState', 'pandaStates', 'peripheralState', 'modelV2', 'liveCalibration',
                                    'carOutput', 'driverMonitoringState', 'longitudinalPlan', 'liveLocationKalman',
                                    'managerState', 'liveParameters', 'radarState', 'liveTorqueParameters',
